d47b41b57d5ff76de39c1bef491d65a06f12bba4
Matias Eyzaguirre <matias@fortlogic.net>
2018-07-31 16:27:54 -0600

    Initial Commit

---
+++ Accelerando/Accelerando.h
@@ -0,0 +1,19 @@
+//
+//  Accelerando.h
+//  Accelerando
+//
+//  Created by Matias Eyzaguirre on 2018-07-31.
+//  Copyright © 2018 Matias Eyzaguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Accelerando.
+FOUNDATION_EXPORT double AccelerandoVersionNumber;
+
+//! Project version string for Accelerando.
+FOUNDATION_EXPORT const unsigned char AccelerandoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Accelerando/PublicHeader.h>
+
+
